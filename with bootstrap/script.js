5dc2b73f9a907db722e42ba2f1cd61169d2f44f7
Simrat Oberoi <simratoberoi2006@gmail.com>
2025-07-07 14:39:47 +0530

    counter added

---
+++ with bootstrap/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.counter-value').each(function(){
+        $(this).prop('counter', 0).animate({
+        Counter: $(this).text()
+        },{
+        duration: 1000,
+        easing: 'swing', 
+        step: function (now){
+        $(this).text(Math.ceil(now));
+        }
+      });
+    
+    });
+});
