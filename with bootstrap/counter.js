cbda9314af9119b9957bbbe127f384180b7814eb
Simrat Oberoi <simratoberoi2006@gmail.com>
2025-07-09 22:28:23 +0530

    landing page complete

--- with bootstrap/counter.js
+++ with bootstrap/counter.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
-    $('.counter-value').each(function(){
-        $(this).prop('counter', 0).animate({
-        Counter: $(this).text()
-        },{
-        duration: 1000,
-        easing: 'swing', 
-        step: function (now){
-        $(this).text(Math.ceil(now));
-        }
-      });
-    
+    $(".counter-value").each(function () {
+      $(this)
+        .prop("counter", 0)
+        .animate(
+          {
+            Counter: $(this).text(),
+          },
+          {
+            duration: 1000,
+            easing: "swing",
+            step: function (now) {
+              $(this).text(Math.ceil(now));
+            },
+          }
+        );
     });
 });
